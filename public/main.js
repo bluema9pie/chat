8e5eeb9b90eccbc63ef6cd8112a5b4bc3466dc00
bluema9pie <bluema9pie@gmail.com>
2016-11-04 05:11:02 +0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -2,7 +2,9 @@ $(function() {
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
-    '#FFFFFF','#FFFFFF', '#FFFFFF', '#FFFFFF'
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
 
   // Initialize varibles
@@ -84,7 +86,7 @@ $(function() {
     }
 
     var $usernameDiv = $('<span class="username"/>')
-      .text(data.username + " /")
+      .text(" " + data.username + " ")
       .css('color', getUsernameColor(data.username));
     var $messageBodyDiv = $('<span class="messageBody">')
       .text(data.message);
